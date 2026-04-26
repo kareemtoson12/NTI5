bc7ec5eae57180c9cd31ec2ee664159e2bf06d20
kareemtoson12 <toson2412@gmail.com>
2026-04-26 12:09:42 +0300

    remove commented-out code and unused NewsCard widget from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,52 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          centerTitle: true,
-          title: Text('News', style: TextStyle(color: Colors.white)),
-        ),
-        body: Column(
-          children: [
-            Text('Pick your category f interest'),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                children: [
-                  NewsCard(color: Colors.redAccent, text: 'football'),
-                  NewsCard(color: Colors.redAccent, text: 'oouyfootball'),
-                  NewsCard(color: Colors.redAccent, text: 'football'),
-                  NewsCard(color: Colors.redAccent, text: 'football'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      home:
     );
   }
 }
 
-class NewsCard extends StatelessWidget {
-  final String text;
-  final Color color;
-  const NewsCard({super.key, required this.text, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      padding: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: color,
-      ),
-      width: 100,
-      height: 200,
-      child: Column(
-        children: [Image.asset('assets/log.png', width: 100), Text(text)],
-      ),
-    );
-  }
-}
