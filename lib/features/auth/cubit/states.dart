00728b477a2a6245ab2879f7dc517e98dfd0059f
kareemtoson12 <toson2412@gmail.com>
2026-05-01 19:26:34 +0300

    Add AuthCubit and AuthState classes for user authentication

---
+++ lib/features/auth/cubit/states.dart
@@ -0,0 +1,33 @@
+// Karim Toson || kareemtoson1@gmail.com || Fri May 01 2026 18:35:01
+
+abstract class AuthState {}
+
+//login
+class LoginIntitalStates extends AuthState {}
+
+class LoginLoadingStates extends AuthState {}
+
+class LoginSuccessStates extends AuthState {
+  final String message;
+  LoginSuccessStates(this.message);
+}
+
+class LoginErrorStates extends AuthState {
+  final String message;
+  LoginErrorStates(this.message);
+}
+
+//signup
+class SignUpIntitalStates extends AuthState {}
+
+class SignUpLoadingStates extends AuthState {}
+
+class SignUpSuccessStates extends AuthState {
+  final String message;
+  SignUpSuccessStates(this.message);
+}
+
+class SignUpErrorStates extends AuthState {
+  final String message;
+  SignUpErrorStates(this.message);
+}
