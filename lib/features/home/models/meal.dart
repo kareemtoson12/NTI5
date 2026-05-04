e760d46ff1712bda0b40644d207cd6a79fff318c
kareemtoson12 <toson2412@gmail.com>
2026-05-04 18:24:36 +0300

    Add meal data handling and update Meal model for Firestore integration

--- lib/features/home/models/meal.dart
+++ lib/features/home/models/meal.dart
@@ -5,11 +5,32 @@ class Meal {
   final String title;
   final String kcal;
   final String time;
+  final String mealType;
 
   Meal({
     required this.image,
     required this.title,
     required this.kcal,
     required this.time,
+    required this.mealType,
   });
+  Map<String, dynamic> toJson() {
+    return {
+      'image': image,
+      'title': title,
+      'kcal': kcal,
+      'time': time,
+      'mealType': mealType,
+    };
+  }
+
+  factory Meal.fromMap(Map<String, dynamic> json) {
+    return Meal(
+      image: json['imag'],
+      kcal: json['kcal'],
+      mealType: json['mealType'],
+      time: json['time'],
+      title: json['title'],
+    );
+  }
 }
